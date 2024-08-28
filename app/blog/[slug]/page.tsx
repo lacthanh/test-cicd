9a42b7a2b36b188d61b132bbfab1cb5cb2071595
lacthanh <92591705+lacthanh@users.noreply.github.com>
2024-08-28 15:49:57 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -11,7 +11,7 @@ export async function generateMetadata({ params }: Params) {
 export default function Page({ params }: Params) {
   return <>
   <h1>Slug: {params.slug}</h1>
-  <p>Husky</p>
+  <p>Hus ky</p>
   <p>Chihuahua, Corgi, Golden, ChouChou, Collie</p>
   </>;
 }
