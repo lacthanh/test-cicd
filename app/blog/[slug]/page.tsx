caf6bcfbd678c3fd65fff97a4889f22c7d97bbd5
lacthanh <92591705+lacthanh@users.noreply.github.com>
2024-08-28 16:16:00 +0700

    update

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -7,7 +7,9 @@ type Params = {
 export async function generateMetadata({ params }: Params) {
   return { title: `Post: ${params.slug}` };
 }
-
+export function generateStaticParams() {
+  return [ { slug: [""] } ]
+}
 export default function Page({ params }: Params) {
   return <>
   <h1>Slug: {params.slug}</h1>
